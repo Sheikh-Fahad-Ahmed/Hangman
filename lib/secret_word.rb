4931d22be1069061093b0cc54aa148853b0d97a8
Fahad Ahmed <fahad838@gmail.com>
2024-10-29 12:05:40 +0530

    Refactored secret_word.rb to a module
    Refactored the secret_word logic into a module
    for using it in a class. also moved it into a
    function called extract word.

--- lib/secret_word.rb
+++ lib/secret_word.rb
@@ -1,10 +1,16 @@
-secret_word = ''
-File.open('dictionary.txt', 'r') do |file|
-  word_array = file.readlines
-  loop do
-    random_word = word_array.sample
-    secret_word = random_word[0..-2]
-    break if secret_word.length >= 5 && secret_word.length <= 12
+# frozen_string_literal: true
+
+module SecretWord
+  def extract_word
+    secret_word = ''
+    File.open('dictionary.txt', 'r') do |file|
+      word_array = file.readlines
+      loop do
+        random_word = word_array.sample
+        secret_word = random_word[0..-2]
+        break if secret_word.length >= 5 && secret_word.length <= 12
+      end
+    end
+    secret_word
   end
-  p secret_word
 end
