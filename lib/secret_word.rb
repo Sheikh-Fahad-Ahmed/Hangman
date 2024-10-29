02222dcd9a97e0bcad866327543409248afe6502
Fahad Ahmed <fahad838@gmail.com>
2024-10-29 10:45:37 +0530

    Extract word from dictionary
    In secret_file.rb, i have implemented a logic to
    extract a word of length between 5 and 12. This
    is the secret code.
    
    I was able to do this by getting a array of all
    the words using readlines() method and using
    sample() method for randomness from the array.
    I had to slice the word as it came with newline
    character(\n).

--- lib/secret_word.rb
+++ lib/secret_word.rb
@@ -1,3 +1,10 @@
-File.open('dictionary.txt','r') do |file|
-  puts file.read()
+secret_word = ''
+File.open('dictionary.txt', 'r') do |file|
+  word_array = file.readlines
+  loop do
+    random_word = word_array.sample
+    secret_word = random_word[0..-2]
+    break if secret_word.length >= 5 && secret_word.length <= 12
+  end
+  p secret_word
 end
