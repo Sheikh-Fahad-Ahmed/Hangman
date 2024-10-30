5bc1618ccc2d4bd24fca5dcb608a95ecb0aaba38
Fahad Ahmed <fahad838@gmail.com>
2024-10-30 18:34:20 +0530

    Add choice_message() and wrong_choice() messages
    added messages for when user has to input a
    choice (choice_message) and for when user enters
    a wrong_choice respectively.

--- lib/display.rb
+++ lib/display.rb
@@ -4,4 +4,12 @@ module Display
   def welcome_message
     puts 'Welcome to Hangman'
   end
+
+  def choice_message
+    puts 'guess a letter: '
+  end
+
+  def wrong_choice
+    puts 'You can only enter 1 letter'
+  end
 end
