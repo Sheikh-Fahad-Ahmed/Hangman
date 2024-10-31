297b719318541996e171127e74751887e3e0a33e
Fahad Ahmed <fahad838@gmail.com>
2024-10-31 11:45:47 +0530

    Add a couple of messages

--- lib/display.rb
+++ lib/display.rb
@@ -9,6 +9,18 @@ module Display
     puts 'guess a letter: '
   end
 
+  def list_of_guesses
+    puts "\nGuessed letters: "
+  end
+
+  def incorrect_guess
+    puts 'Your guess is incorrect'
+  end
+
+  def letter_guessed
+    puts 'You have already guessed this letter'
+  end
+
   def wrong_choice
     puts 'You can only enter 1 letter'
   end
