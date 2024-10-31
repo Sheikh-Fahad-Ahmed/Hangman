f6837224b4be72813cab33411f67110717dbf589
Fahad Ahmed <fahad838@gmail.com>
2024-10-31 11:22:52 +0530

    Create a check_choice function with choice as arg
    check_choice(choice) first checks for if the
    letter is already guessed and shows a message for
    it. if the letter is not guessed it is added
    to the guessed_letters array and then checks
    if it is included in secret_word. If the letter
    exists in the secret_word all instances of that
    letter are then put up on the board. If the letter
    does not exist then incorrect_letter message is
    shown and guesses counter is decremented.

--- lib/game.rb
+++ lib/game.rb
@@ -4,13 +4,16 @@ require_relative 'secret_word'
 require_relative 'display'
 
 class Game
-  attr_reader :secret_word, :player, :board
+  attr_accessor :board, :guessed_letters, :guesses
+  attr_reader :secret_word, :player
 
   include SecretWord
   include Display
   def initialize(player)
     @secret_word = extract_word
     @player = player
+    @guesses = 11
+    @guessed_letters = []
     @board = Array.new(secret_word.length) { '_' }
   end
 
@@ -26,17 +29,34 @@ class Game
     puts 'You get 11 tries'
   end
 
+  def check_choice(choice)
+    if guessed_letters.include?(choice)
+      letter_guessed
+    else
+      guessed_letters.push(choice)
+      if secret_word.include?(choice)
+        secret_word.split('').each_with_index do |item, index|
+          board[index] = choice if item == choice
+        end
+      else
+        incorrect_guess
+        @guesses -= 1
+      end
+    end
+  end
+
   def game_loop
-    guesses = 11
-    until guesses == 0
+    until guesses.zero?
       choice_message
+      puts board.join(' ')
+      puts guessed_letters.join(' ')
       choice = gets.chomp.downcase
       until choice.match(/^[a-z]$/)
         wrong_choice
         choice_message
         choice = gets.chomp.downcase
       end
-      
+      check_choice(choice)
     end
   end
 end
